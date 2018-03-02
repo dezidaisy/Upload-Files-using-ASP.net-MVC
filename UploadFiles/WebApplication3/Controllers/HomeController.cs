4c9b47aacbb51ddd6b5b1ffe30762d12448c8822
dezidaisy <32540506+dezidaisy@users.noreply.github.com>
2018-03-02 15:43:52 +0000

    COMMIT

--- UploadFiles/WebApplication3/Controllers/HomeController.cs
+++ UploadFiles/WebApplication3/Controllers/HomeController.cs
@@ -6,12 +6,15 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using WebApplication3.Models;
 using System.IO;
+using Microsoft.AspNetCore.Http;
 
-namespace WebApplication3.Controllers
+namespace WebApplication3
 {
+
     public class HomeController : Controller
     {
-        public IActionResult Index()
+        public static UFiles FileList = new UFiles();
+        public  IActionResult Index()
         {
             return View();
         }
@@ -20,7 +23,42 @@ namespace WebApplication3.Controllers
         {
             return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
         }
-       
-         
+        
+
+
+        [HttpPost("UploadFiles")]
+        public async Task<IActionResult> Post(List<IFormFile> files)
+        {
+
+            long size = 0;
+            // full path to file in temp location
+            var filePath = "";
+
+            if (files != null)
+            {
+                size = files.Sum(f => f.Length);
+                FileList.Size = size;
+                FileList.Count = files.Count;
+                foreach (var formFile in files)
+                {
+                    filePath = Path.Combine(Directory.GetCurrentDirectory() + "\\Documents\\" + formFile.FileName);
+                    FileList.FileName.Add(filePath);
+                    if (formFile.Length > 0)
+                    {
+                        using (var stream = new FileStream(filePath, FileMode.Create))
+                        {
+                            await formFile.CopyToAsync(stream);
+                        }
+                    }
+                }
+            }
+
+            // process uploaded files
+
+
+            return View("Index");
+            //return Ok(new { count = FileList.Count });
+        }
+
     }
 }
---
+++ UploadFiles/WebApplication3/Models/UFiles.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApplication3
+{
+    public class UFiles
+    {
+        public List<string> FileName = new List<string>() ;
+        public long Size { get; set; }
+        public  int Count { get; set; }
+        
+    }
+
+
+
+}
